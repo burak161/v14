a9c279bdcac6b3753b23a7f231f7c7565fe126ad
burak <65681772+burak161@users.noreply.github.com>
2023-04-07 01:30:25 +0300

    Add files via upload

---
+++ src/commands/leave.js
@@ -0,0 +1,19 @@
+const { EmbedBuilder, PermissionsBitField } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const Discord = require("discord.js")
+const db = require("croxydb")
+const languagefile = require("../language.json")
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("leave")
+    .setDescription("🎵 | Müziği Bitir!"),
+    run: async (client, interaction) => {
+      await interaction.deferReply().catch(err => {})
+      const queue = client.distube.getQueue(interaction);
+         if (!queue) return interaction.followUp(`Listede henüz şarkı yok.`)
+         client.distube.voices.leave(interaction)
+         await interaction.followUp("Ses kanallarını bırakıyorum.").catch(err => {})
+         db.delete(`music_${interaction.guild.id}`)
+return;
+ }
+}
---
+++ src/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "token": "MTA4MzEzODIxMjg2NTc5MDAzMw.Grc5Rs.-lY0hi63V91NDWvR8pHrfEnWTFMTzRSdb3JE0U",
+  "language": true
+}
